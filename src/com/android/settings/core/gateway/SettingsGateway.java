crdroidandroid/android_packages_apps_Settings

GamingMode: Long press tile to open settings [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -24,6 +24,7 @@ import com.android.settings.MasterClear;
 import com.android.settings.PrivacySettings;
 import com.android.settings.Settings;
 import com.android.settings.Settings.FlingSettingsActivity;
+import com.android.settings.Settings.GamingModeActivity;
 import com.android.settings.Settings.PieSettingsActivity;
 import com.android.settings.Settings.PulseSettingsActivity;
 import com.android.settings.Settings.SmartbarSettingsActivity;
@@ -144,6 +145,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.crdroid.settings.crDroidSettingsLayout;
+import com.crdroid.settings.fragments.misc.GamingMode;
 
 public class SettingsGateway {
 
@@ -273,6 +275,7 @@ public class SettingsGateway {
             crDroidSettingsLayout.class.getName(),
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
+            GamingMode.class.getName(),
             PieSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName(),
             RunningServices.class.getName()
@@ -317,6 +320,7 @@ public class SettingsGateway {
             Settings.crDroidSettingsLayoutActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
             Settings.PieSettingsActivity.class.getName(),
             Settings.PulseSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName()

dinosnore1/packages_apps_Settings

Change-Id: I9ff4fe74172ea2bddbc81ae8fb67e1bbf55af16b
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -25,6 +25,7 @@ import com.android.settings.PrivacySettings;
 import com.android.settings.Settings;
 import com.android.settings.Settings.FlingSettingsActivity;
 import com.android.settings.Settings.NavigationSettingsActivity;
+import com.android.settings.Settings.GamingModeActivity;
 import com.android.settings.Settings.PulseSettingsActivity;
 import com.android.settings.Settings.SmartbarSettingsActivity;
 import com.android.settings.TestingSettings;
@@ -147,6 +148,7 @@ import com.android.settings.livedisplay.LiveDisplaySettings;
 import com.bootleggers.dumpster.MainDump;
 import com.bootleggers.dumpster.fragments.PowerMenuSettings;
 import com.bootleggers.dumpster.fragments.BootlegWelcome;
+import com.bootleggers.dumpster.fragments.GamingMode;
 
 public class SettingsGateway {
 
@@ -280,8 +282,9 @@ public class SettingsGateway {
             NavigationSettingsActivity.class.getName(),
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
-            PulseSettingsActivity.class.getName()
+            PulseSettingsActivity.class.getName(),
             LiveDisplaySettings.class.getName(),
+            GamingMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -325,6 +328,7 @@ public class SettingsGateway {
             Settings.NavigationSettingsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(), 
-            Settings.PulseSettingsActivity.class.getName()
+            Settings.PulseSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName()
     };
 }